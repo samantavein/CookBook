3da57b35af0bf32b05d1a5b913ee0b0ed43275ee
samantavein <samantavein@gmail.com>
2023-02-18 13:12:54 +0200

    nineth commit

--- src/Pages/CreateRecipe.js
+++ src/Pages/CreateRecipe.js
@@ -27,6 +27,10 @@ const CreateRecipeLabel = styled.label`
   margin-bottom: 10px;
   font-family: Arial, sans-serif;
   color: #5e5e5e;
+  ${props => props.small && css`
+  font-size: 14px;
+  margin-bottom: 10px;
+`}
 `;
 const CreateRecipeInput = styled.input`
   width: 100%;
@@ -50,15 +54,29 @@ const CreateRecipeTextarea = styled.textarea`
 const CreateRecipe = () => {
   const [recipes, setRecipes] = useContext(RecipeContext);
   const [ingredients, setIngredients] = useState([]);
+  const [ingredientInput, setIngredientInput] = useState('');
+
+  const handleIngredientInput = (e) => {
+    setIngredientInput(e.target.value);
+  };
 
   const handleAddIngredient = (e) => {
     e.preventDefault();
-    setIngredients([...ingredients, e.target.ingredient.value]);
-    e.target.ingredient.value = "";
+    setIngredients([...ingredients, ingredientInput]);
+    setIngredientInput('');
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const title = e.target.title.value.trim();
+    const method = e.target.method.value.trim();
+    const time = parseInt(e.target.time.value.trim(), 10);
+
+    if (!title || !method || isNaN(time)) {
+      alert("All fields should be filled and time should be a number!");
+      return;
+    }
 
     const res = await fetch("http://localhost:4001/recipes", {
       method: "POST",
@@ -84,18 +102,19 @@ const CreateRecipe = () => {
 
   return (  
     <Container onSubmit={handleSubmit}>
-      <h2 style = {{ textAlign:"center" }}>Add a new recipe</h2>
+      <h2 style={{ textAlign: "center" }}>Add a new recipe</h2>
       <CreateRecipeLabel>Recipe title:</CreateRecipeLabel>
       <CreateRecipeInput type="text" name="title" />
-      <CreateRecipeLabel>Recipe ingredients:</CreateRecipeLabel>
+      <CreateRecipeLabel >Recipe ingredients:</CreateRecipeLabel>
+      <CreateRecipeLabel small>Current ingredients: </CreateRecipeLabel>
       <div >
         {ingredients.map((ingredient, index) => (
           <div key={index}>{ingredient}</div>
         ))}
-        <CreateRecipeInput width="true" type="text" name="ingredient" />
-        <CreateButton margin onClick={handleAddIngredient}>add</CreateButton>
+        <CreateRecipeInput width="true" type="text" name="ingredient" value={ingredientInput} onChange={handleIngredientInput} />
+        <CreateButton margin onClick={handleAddIngredient}>add</CreateButton>      
       </div>
-      
+
       <CreateRecipeLabel>Recipe method:</CreateRecipeLabel>
       <CreateRecipeTextarea type="textarea" name="method" />
       <CreateRecipeLabel>Cooking time (in minutes):</CreateRecipeLabel>
@@ -107,199 +126,3 @@ const CreateRecipe = () => {
   );
 };
 export default CreateRecipe;
-
-
-/*
-import React, { useContext } from "react";
-import { RecipeContext } from "../RecipeContext";
-
-const CreateRecipe = () => {
-  const [recipes, setRecipes] = useContext(RecipeContext);
-
-  
-  const addRecipe = async (e) => {
-    e.preventDefault();
-
-    // send POST request to the server to add a new recipe
-    const res = await fetch("http://localhost:4001/recipes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: e.target.title.value,
-        ingredients: e.target.ingredients.value,
-        method: e.target.method.value,
-        time: e.target.time.value,
-      }),
-    });
-
-    // get the newly added recipe from the server and add it to the context
-    const newRecipe = await res.json();
-    setRecipes([...recipes, newRecipe]);
-
-    // clear the form
-    e.target.title.value = "";
-    e.target.ingredients.value = "";
-    e.target.method.value = "";
-    e.target.time.value = "";
-  };
-
-  
-  return (
-      <form >
-        <label>Recipe title:</label>
-        <input 
-          type="text" 
-          name="title" 
-        />
-        <label>Recipe ingredients:</label>
-        <input
-          type="text"
-          name="ingredients" 
-        />
-        <button>add</button>
-        <label>Recipe method:</label>
-         <textarea
-          type="textarea"
-          name="method"       
-        />
-        <label>Cooking time (in minutes):</label>
-        <input
-        type="text"
-        name="time"
-  
-        />
-        <button type="submit" onSubmit={addRecipe}>submit</button>
-      </form>
-  )
-};
-
-export default CreateRecipe;
-*/
-
-/*
-import React, { useContext, useState } from "react";
-import { RecipeContext } from "../RecipeContext";
-
-const CreateRecipe = () => {
-  const [recipes, setRecipes] = useContext(RecipeContext);
-  const [ingredients, setIngredients] = useState([]);
-
-  const handleAddIngredient = (e) => {
-    e.preventDefault();
-    const newIngredient = e.target.ingredient.value;
-    setIngredients([...ingredients, newIngredient]);
-    e.target.ingredient.value = "";
-  };
-
-  const addRecipe = async (e) => {
-    e.preventDefault();
-
-    // send POST request to the server to add a new recipe
-    const res = await fetch("http://localhost:4001/recipes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: e.target.title.value,
-        ingredients: ingredients,
-        method: e.target.method.value,
-        time: e.target.time.value,
-      }),
-    });
-
-    // get the newly added recipe from the server and add it to the context
-    const newRecipe = await res.json();
-    setRecipes([...recipes, newRecipe]);
-
-    // clear the form
-    e.target.title.value = "";
-    setIngredients([]);
-    e.target.method.value = "";
-    e.target.time.value = "";
-  };
-
-  return (
-    <form >
-      <label>Recipe title:</label>
-      <input type="text" name="title" />
-      <label>Recipe ingredients:</label>
-      <ul>
-        {ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
-      <form >
-        <input type="text" name="ingredient" />
-        <button type="submit">add</button>
-      </form>
-      <label>Recipe method:</label>
-      <textarea type="textarea" name="method" />
-      <label>Cooking time (in minutes):</label>
-      <input type="text" name="time" />
-      <button type="submit" onSubmit={addRecipe}>submit</button>
-    </form>
-  );
-};
-
-export default CreateRecipe;
-*/
-
-/*
-import React, { useContext } from "react";
-import { RecipeContext } from "../RecipeContext";
-
-const CreateRecipe = () => {
-  const [recipes, setRecipes] = useContext(RecipeContext);
-
-  const addRecipe = e => {
-    e.preventDefault();
-    setRecipes([
-      ...recipes,
-      {
-        title: e.target.title.value,
-        ingredients: e.target.ingredients.value,
-        method: e.target.method.value,
-        time: e.target.time.value
-
-      }
-    ]);
-    e.target.title.value = "";
-    e.target.ingredients.value = "";
-    e.target.method.value = "";
-    e.target.time.value = "";
-  };
-
-  return (
-    <form >
-      <label>Recipe title:</label>
-      <input 
-        type="text" 
-        name="title" 
-      />
-      <label>Recipe ingredients:</label>
-      <input
-        type="text"
-        name="ingredients" 
-      />
-      <button>add</button>
-      <label>Recipe method:</label>
-       <textarea
-        type="textarea"
-        name="method"       
-      />
-      <label>Cooking time (in minutes):</label>
-      <input
-      type="text"
-      name="time"
-
-      />
-      <button type="submit" onSubmit={addRecipe}>submit</button>
-    </form>
-  );
-};
-
-export default CreateRecipe;
-*/
